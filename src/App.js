77d6eac6ec102243c5fbc9183966c383e88e1ef6
j23saw <jeshsawant23@gmail.com>
2021-01-31 23:47:56 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import ReactTooltip from "react-tooltip";
+import MapChart from "./MapChart";
 import './App.css';
 
 function App() {
+  const [content, setContent] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Map">
+        <MapChart setTooltipContent={setContent} />
+        <ReactTooltip>{content}</ReactTooltip>
+      </div>
+      <div>
+        TopSpot
+      </div>
     </div>
   );
 }
