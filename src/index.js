04301559e4fb680e0305a761cb30e9011f89504c
j23saw <jeshsawant23@gmail.com>
2021-02-10 20:51:26 +0530

    add: available markets

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as d3 from "d3-dsv";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+fetch("	https://spotifycharts.com/regional/us/daily/latest/download")
+  .then((response) => response.text())
+  .then((data) => {
+    let string =
+      ',,,"Note that these figures are generated using a formula that protects against any artificial inflation of chart positions.",\n';
+    data = data.replace(string, "");
+    console.table(d3.csvParse(data));
+  });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
